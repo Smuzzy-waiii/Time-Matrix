6b0b38300426d3855b9540c40ad3ced02f5996f6
Smuzzy_waiii <smaran.jawalkar@gmail.com>
2021-05-19 23:59:31 +0530

    grid barebones ui made

--- lib/main.dart
+++ lib/main.dart
@@ -20,29 +20,55 @@ class _HomeState extends State<Home> {
 
 List<Widget> generateGridColumns() {
   List<Widget> buttonrows = [];
+  int buttonsize = 40;
+
   for (var i = 0; i < 24; i++) {
     buttonrows.add(Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Container(
           child: Text("${(i + 1) < 10 ? '0${i + 1}:00' : '${i + 1}:00'}"),
         ),
-        RaisedButton(
-          onPressed: () {},
-          //child: Text("${i + 1}:00-${i + 1}:15"),
+        Container(
+          height: 30,
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: RaisedButton(
+              onPressed: () {},
+              //child: Text("${i + 1}:00-${i + 1}:15"),
+            ),
+          ),
         ),
-        RaisedButton(
-          onPressed: () {},
-          //child: Text("${i + 1}:15-${i + 1}:30"),
+        Container(
+          height: 30,
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: RaisedButton(
+              onPressed: () {},
+              //child: Text("${i + 1}:00-${i + 1}:15"),
+            ),
+          ),
         ),
-        RaisedButton(
-          onPressed: () {},
-          //child: Text("${i + 1}:30-${i + 1}:45"),
+        Container(
+          height: 30,
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: RaisedButton(
+              onPressed: () {},
+              //child: Text("${i + 1}:00-${i + 1}:15"),
+            ),
+          ),
         ),
-        RaisedButton(
-          onPressed: () {},
-          //child: Text("${i + 1}:45-${i + 2}:00"),
+        Container(
+          height: 30,
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: RaisedButton(
+              onPressed: () {},
+              //child: Text("${i + 1}:00-${i + 1}:15"),
+            ),
+          ),
         )
       ],
     ));
@@ -58,8 +84,42 @@ class Grid extends StatefulWidget {
 class _GridState extends State<Grid> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: generateGridColumns(),
+    return Container(
+      padding: EdgeInsetsDirectional.all(5),
+      child: ListView.separated(
+          separatorBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              height: 5,
+            );
+          },
+          itemCount: 24,
+          itemBuilder: (_, i) {
+            return Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.center,
+                    color: Colors.grey[350],
+                    padding: EdgeInsets.fromLTRB(0, 35, 5, 35),
+                    child: Text(
+                        "${(i + 1) < 10 ? '0${i + 1}:00' : '${i + 1}:00'}"),
+                  ),
+                  for (i = 0; i < 4; i++) ...[
+                    Expanded(
+                      child: AspectRatio(
+                        aspectRatio: 1,
+                        child: RaisedButton(
+                          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+                          onPressed: () {},
+                          //child: Text("${i + 1}:00-${i + 1}:15"),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 5)
+                  ],
+                ]);
+          }),
     );
   }
 }

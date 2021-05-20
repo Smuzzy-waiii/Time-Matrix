76fd1d43654de1f0ca38836eb44fff7690110b8d
Smuzzy_waiii <smaran.jawalkar@gmail.com>
2021-05-20 15:38:47 +0530

    baseplare ui grid MADE

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Home(),
+    theme: new ThemeData(scaffoldBackgroundColor: Colors.black),
   ));
 }
 
@@ -28,7 +29,9 @@ List<Widget> generateGridColumns() {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Container(
-          child: Text("${(i + 1) < 10 ? '0${i + 1}:00' : '${i + 1}:00'}"),
+          child: Text(
+            "${(i + 1) < 10 ? '0${i + 1}:00' : '${i + 1}:00'}",
+          ),
         ),
         Container(
           height: 30,
@@ -84,42 +87,60 @@ class Grid extends StatefulWidget {
 class _GridState extends State<Grid> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsetsDirectional.all(5),
-      child: ListView.separated(
-          separatorBuilder: (BuildContext context, int index) {
-            return SizedBox(
-              height: 5,
-            );
-          },
-          itemCount: 24,
-          itemBuilder: (_, i) {
-            return Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.center,
-                    color: Colors.grey[350],
-                    padding: EdgeInsets.fromLTRB(0, 35, 5, 35),
-                    child: Text(
-                        "${(i + 1) < 10 ? '0${i + 1}:00' : '${i + 1}:00'}"),
-                  ),
-                  for (i = 0; i < 4; i++) ...[
-                    Expanded(
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: RaisedButton(
-                          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-                          onPressed: () {},
-                          //child: Text("${i + 1}:00-${i + 1}:15"),
+    return Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Container(
+            padding: EdgeInsetsDirectional.all(5),
+            child: ListView.separated(
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(
+                    height: 3,
+                  );
+                },
+                itemCount: 24,
+                itemBuilder: (_, i) {
+                  return Row(
+                      //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            alignment: Alignment.center,
+                            color: Colors.grey[350],
+                            padding: EdgeInsets.fromLTRB(0, 1, 1, 0),
+                            child: Text(
+                              "${(i) < 10 ? '0${i}:00' : '${i + 1}:00'} AM",
+                              //style: TextStyle(fontSize: 10),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    SizedBox(width: 5)
-                  ],
-                ]);
-          }),
+                        for (i = 0; i < 4; i++) ...[
+                          Expanded(
+                            child: AspectRatio(
+                              aspectRatio: 1.5,
+                              child: RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                onPressed: () {},
+                                color: Colors.white,
+                                child: Text(
+                                  "00-15",
+                                  style: TextStyle(fontSize: 5),
+                                ),
+                              ),
+                            ),
+                          ),
+                          if (i < 3) SizedBox(width: 3)
+                        ],
+                      ]);
+                }),
+          ),
+        ),
+        Spacer(),
+      ],
     );
   }
 }

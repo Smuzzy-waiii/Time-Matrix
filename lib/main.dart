ac4c7749091d8a69ab78d22ff99777b4ec302a2c
Smuzzy_waiii <smaran.jawalkar@gmail.com>
2021-05-22 18:08:14 +0530

    Side buttons added

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
-    //theme: new ThemeData(scaffoldBackgroundColor: Colors.black),
-  ));
+      home: Home(),
+      theme: new ThemeData(
+          primaryColor: Colors.orange,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(primary: Colors.blue)))));
 }
 
 class Home extends StatefulWidget {
@@ -18,7 +20,8 @@ class _HomeState extends State<Home> {
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-            backgroundColor: Colors.yellow[900], title: Text("My Time App")),
+            /*backgroundColor: Colors.yellow[900],*/ title:
+                Text("My Time App")),
         body: Grid());
   }
 }
@@ -30,12 +33,14 @@ class Grid extends StatefulWidget {
 
 int gi_to_li(int i, int j) {
   //Grid indices to List index
-  int top_pos = j + 1;
-  int side_pos = i + 1;
-  return 4 * (side_pos - 1) + top_pos;
+  int topPos = j + 1;
+  int sidePos = i + 1;
+  return 4 * (sidePos - 1) + topPos - 1;
 }
 
 class _GridState extends State<Grid> {
+  bool fillmode = false;
+  Color fillcolor = Colors.red;
   List<Color> colordata = List<Color>.generate(24 * 4, (index) => null);
   List<Color> colors = [
     Colors.white,
@@ -46,6 +51,14 @@ class _GridState extends State<Grid> {
     Colors.yellow,
     Colors.white
   ];
+  Map color_names = {
+    Colors.white: 'white',
+    Colors.red: 'Red',
+    Colors.green: 'Green',
+    Colors.blue: 'Blue',
+    Colors.orange: 'Orange',
+    Colors.yellow: 'Yellow',
+  };
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -92,25 +105,33 @@ class _GridState extends State<Grid> {
                             Expanded(
                               child: AspectRatio(
                                 aspectRatio: 1.5,
-                                child: RaisedButton(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0)),
+                                child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(9.0)),
+                                        primary:
+                                            (colordata[gi_to_li(i, j)] != null)
+                                                ? colordata[gi_to_li(i, j)]
+                                                : Colors.white),
                                     onPressed: () {
                                       setState(() {
-                                        print("${i + 1}, ${j + 1}");
-                                        Color curr_color =
+                                        Color currColor =
                                             (colordata[gi_to_li(i, j)] != null)
                                                 ? colordata[gi_to_li(i, j)]
                                                 : Colors.white;
-                                        int pos = colors.indexOf(curr_color);
-                                        colordata[gi_to_li(i, j)] =
-                                            colors[pos + 1];
+                                        int pos = colors.indexOf(currColor);
+                                        if (fillmode) {
+                                          colordata[gi_to_li(i, j)] = fillcolor;
+                                        } else {
+                                          colordata[gi_to_li(i, j)] =
+                                              colors[pos + 1];
+                                        }
                                       });
                                     },
-                                    color: (colordata[gi_to_li(i, j)] != null)
+                                    /*color: (colordata[gi_to_li(i, j)] != null)
                                         ? colordata[gi_to_li(i, j)]
-                                        : Colors.white,
+                                        : Colors.white,*/
                                     child: i != -1
                                         ? Container()
                                         : FittedBox(
@@ -139,7 +160,71 @@ class _GridState extends State<Grid> {
                 }),
           ),
         ),
-        Spacer(),
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FittedBox(
+                  fit: BoxFit.contain,
+                  child: ElevatedButton(
+                    child: Text("Clear all"),
+                    onPressed: () {
+                      setState(() {
+                        colordata = List.generate(24 * 4, (index) => null);
+                      });
+                    },
+                  ),
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Save")),
+                Text(
+                  "Fill Mode:",
+                  style: TextStyle(color: Colors.white),
+                ),
+                ElevatedButton(
+                  child: Text("${fillmode ? 'ON' : 'OFF'}"),
+                  onPressed: () {
+                    setState(() {
+                      fillmode = !fillmode;
+                    });
+                  },
+                ),
+                if (fillmode)
+                  Text(
+                    "Fill Color:",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ElevatedButton(
+                  child: Text(
+                    "${color_names[fillcolor]}",
+                    style: TextStyle(color: () {
+                      if (fillmode) {
+                        return fillcolor.computeLuminance() > 0.5
+                            ? Colors.black
+                            : Colors.white;
+                      } else {
+                        return Colors.black;
+                      }
+                    }()),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: fillcolor,
+                  ),
+                  onPressed: !fillmode
+                      ? null
+                      : () {
+                          setState(() {
+                            int index = colors.indexOf(fillcolor);
+                            fillcolor = colors[index + 1];
+                          });
+                        },
+                )
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
